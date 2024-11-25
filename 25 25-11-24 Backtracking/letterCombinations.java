6d457a2a784cbb4f12686005b3115ce03c03cb16
VarunReddyT <tvarun2014@gmail.com>
2024-11-25 20:46:08 +0530

    New

---
+++ 25 25-11-24 Backtracking/letterCombinations.java
@@ -0,0 +1,44 @@
+// Given a classic mobile phone, and the key pad of the phone looks like below.
+// 	1		2		3
+//            abc	   def
+		 
+// 	4		5		6
+//     ghi    jkl     mno
+  
+// 	7		8		9
+//     pqrs    tuv     wxyz
+	
+// 	*		0		#
+
+
+// You are given a string S contains digits between [2-9] only, 
+// For example: S = "2", then the possible words are "a", "b", "c".
+
+// Now your task is to find all possible words that the string S could represent.
+// and print them in a lexicographical order. 
+
+// Input Format:
+// -------------
+// A string S, consist of digits [2-9]
+
+// Output Format:
+// --------------
+// Print the list of words in lexicographical order.
+
+
+// Sample Input-1:
+// ---------------
+// 2
+
+// Sample Output-1:
+// ----------------
+// [a, b, c]
+
+
+// Sample Input-2:
+// ---------------
+// 24
+
+// Sample Output-2:
+// ----------------
+// [ag, ah, ai, bg, bh, bi, cg, ch, ci]
